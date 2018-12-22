f21c59215734567b3dc3aa610a2f4f84a712632c
Guilherme Henrique <guilhermeerba@hotmail.com>
2018-12-22 00:31:25 -0200

    Menu Mobile Adicionado

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,16 +1,10 @@
 export default function initMenuMobile() {
   const btnOpenMenu = document.querySelector('[data-menu="open"]');
-  const btnCloseMenu = document.querySelector('[data-menu="close"]');
   const menu = document.querySelector('[data-menu="menu"]');
 
   function openMenu() {
-    menu.classList.add('active');
-  }
-
-  function closeMenu() {
-    menu.classList.remove('active');
+    menu.classList.toggle('active');
   }
 
   btnOpenMenu.addEventListener('click', openMenu);
-  btnCloseMenu.addEventListener('click', closeMenu);
 }
