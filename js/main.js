030447b78a42e37e4ea10bcac60b8501c5d9e42e
Guilherme Henrique <guilhermeerba@hotmail.com>
2020-02-26 14:08:29 -0300

    pagina de manutencao

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import initMenuMobile from './modules/menu-mobile.js';
-import SlideNav from './modules/slide.js';
-
-initMenuMobile();
-const slide = new SlideNav('.slide', '.slide-wrapper');
-slide.init();
-slide.addArrow('.prev', '.next');
-slide.addControl('.custom-controls');
