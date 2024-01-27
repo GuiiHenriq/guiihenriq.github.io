27fb2a3573ca755ceb5fc6a93493e7c2654814dd
GuiiHenriq <guilhermeerba@hotmail.com>
2024-01-27 11:34:17 -0300

    :sparkles: feat: integrate form contact with discord

--- app/components/contact-form/index.tsx
+++ app/components/contact-form/index.tsx
@@ -6,6 +6,8 @@ import { SectionTitle } from '../section-title'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 const contactFormSchema = z.object({
   name: z.string().min(3).max(100),
@@ -16,12 +18,23 @@ const contactFormSchema = z.object({
 type ContactFormData = z.infer<typeof contactFormSchema>
 
 export const ContactForm = () => {
-  const { handleSubmit, register } = useForm<ContactFormData>({
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<ContactFormData>({
     resolver: zodResolver(contactFormSchema),
   })
 
-  const onSubmit = (data: ContactFormData) => {
-    console.log(data)
+  const onSubmit = async (data: ContactFormData) => {
+    try {
+      await axios.post('/api/contact', data)
+      toast.success('Message sent successfully!')
+      reset()
+    } catch {
+      toast.error('Error sending your message, please try again!')
+    }
   }
 
   return (
@@ -61,7 +74,10 @@ export const ContactForm = () => {
             {...register('message')}
           />
 
-          <Button className="w-max mx-auto mt-6 shadow-button">
+          <Button
+            className="w-max mx-auto mt-6 shadow-button"
+            disabled={isSubmitting}
+          >
             Send <HiArrowNarrowRight size={18} />
           </Button>
         </form>
