1cc711433c1f7acf47fe4065e36c7384490413ce
GuiiHenriq <guilhermeerba@hotmail.com>
2024-03-26 18:48:36 -0300

    :sparkles: feat: Add link to Github for footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
+
 export const Footer = () => {
   return (
     <footer className="h-14 w-full flex items-center justify-center bg-gray-950">
-      <span className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400 font-semibold">
+      <Link
+        href="https://github.com/GuiiHenriq"
+        target="_blank"
+        className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400 font-semibold underline hover:text-gray-300 transition-colors"
+      >
         Guilherme Henrique
-      </span>
+      </Link>
     </footer>
   )
 }
