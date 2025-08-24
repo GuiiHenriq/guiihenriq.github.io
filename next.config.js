04d200025994095023d46e240615c01ac2791929
GuiiHenriq <guilhermeerba@hotmail.com>
2025-08-24 16:37:09 -0300

    :package: build: Update version Next, React and dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['media.graphassets.com'],
+    domains: ['media.graphassets.com', 'sa-east-1.graphassets.com'],
   },
 }
 
